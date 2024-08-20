ee300d1989ac1103e0580f94650bcd3157eaf8e1
MinmusLin <minmuslin@outlook.com>
2024-08-20 14:04:35 +0800

    +

---
+++ src/vite-env.d.ts
@@ -0,0 +1,6 @@
+declare module '*.vue' {
+  import type {DefineComponent} from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  // noinspection JSUnusedGlobalSymbols
+  export default component
+}
