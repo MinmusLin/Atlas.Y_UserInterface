55dfaba5acf5e1d0f4c2d0667b2bdd3805e6d7f6
MinmusLin <minmuslin@outlook.com>
2024-08-20 14:43:59 +0800

    +

---
+++ src/pages/FeedbackPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class='page-container'>
+    <p>FeedbackPage</p>
+  </div>
+</template>
+
+<script setup lang='ts'>
+</script>
+
+<style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
