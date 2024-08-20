1bdd0f0d82e0b59c058e237108dcbcb0a1156cef
MinmusLin <minmuslin@outlook.com>
2024-08-20 14:46:25 +0800

    Update components.d.ts

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Navbar: typeof import('./src/components/Navbar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
