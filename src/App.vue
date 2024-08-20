c24d57e52262c2aae6b2013037e0835bb3f9a792
MinmusLin <minmuslin@outlook.com>
2024-08-20 14:41:06 +0800

    +

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-main class='main-container'>
+      <Navbar/>
+      <div class='content-container'>
+        <router-view class='router-view-container'/>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+</script>
+
+<style scoped>
+.main-container {
+  display: flex;
+  height: 100vh;
+}
+
+.content-container {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.router-view-container {
+  width: 1190px;
+  height: 800px;
+  border: 1px solid black;
+  padding: 20px;
+  box-sizing: border-box;
+}
+</style>
