8bc2089d1eb30a895fe491155bd636c7d173f249
MinmusLin <minmuslin@outlook.com>
2024-08-20 19:02:34 +0800

    Create ShadowButton.vue

---
+++ src/components/ShadowButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class='start-button'>
+    <slot/>
+  </button>
+</template>
+
+<script setup lang='ts'>
+import {defineProps} from 'vue'
+
+interface ButtonProps {
+  width: string;
+  height: string;
+}
+
+defineProps<ButtonProps>();
+</script>
+
+<style scoped>
+.start-button {
+  height: v-bind(height);
+  width: v-bind(width);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #2F62D7;
+  color: white;
+  font-weight: bold;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: box-shadow 0.3s;
+  font-size: 25px;
+}
+
+.start-button:hover {
+  box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.2)
+}
+</style>
