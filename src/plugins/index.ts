356b4550896acfb5fd6b67625303767250a4e35a
MinmusLin <minmuslin@outlook.com>
2024-08-20 14:13:38 +0800

    +

---
+++ src/plugins/index.ts
@@ -0,0 +1,7 @@
+import vuetify from './vuetify'
+import router from '../router'
+import type {App} from 'vue'
+
+export function registerPlugins(app: App) {
+  app.use(vuetify).use(router)
+}
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,9 @@
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+
+export default createVuetify({
+  theme: {
+    defaultTheme: 'light'
+  }
+})
