0de38c267d1bc3efbfe55c401f4e6bd02023c04b
MinmusLin <minmuslin@outlook.com>
2024-08-29 10:56:13 +0800

    +

--- src/pages/WikiPage.vue
+++ src/pages/WikiPage.vue
@@ -1,10 +1,46 @@
 <template>
   <div class='page-container'>
-    <p>WikiPage</p>
+    <div style='display: flex; flex-direction: column; align-items: center'>
+      <p style='color: #2F3235; font-size: 40px; font-weight: 600'>Our WIKI provides a comprehensive overview of</p>
+      <p style='color: #7AA8F3; font-size: 40px; font-weight: 600'>Atlas.Y toolkit's development</p>
+      <div style='display: flex; flex-direction: row; margin-top: 8px'>
+        <p>Our WIKI covers every link from&nbsp;</p>
+        <p style='color: #AACAF9'>the concept of topic selection, design principle,</p>
+      </div>
+      <p style='color: #AACAF9'>algorithm implementation to the actual construction.</p>
+      <div style='display: flex; flex-direction: row'>
+        <p>Besides,&nbsp;</p>
+        <p style='color: #AACAF9'>the advocacy strategy and human practices</p>
+        <p>&nbsp;are also documented in detail.</p>
+      </div>
+      <p style='margin-top: 10px'>By visiting the WIKI, you can gain insight into the entire development process,</p>
+      <p>explore our technical details and the thinking behind our decisions,</p>
+      <p>and access a wealth of background information and resource support.</p>
+      <div style='display: flex; flex-direction: row; margin-top: 35px'>
+        <a href='https://2024.igem.wiki/tongji-software'
+           style='color: #5182F8; font-size: 40px; font-weight: 600; text-decoration: none; position: relative'>
+          CLICK HERE
+          <span style='position: absolute; bottom: 5px; left: 0; width: 100%; height: 3px; background-color: #5182F8'/>
+        </a>
+        <p style='color: #2F3235; font-size: 40px; font-weight: 600; transform: translateY(6px)'>
+          &nbsp;to visit our WIKI
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang='ts'>
+import {ref} from 'vue'
+
+const images = ref([
+  {src: '@/assets/Wiki/Demo.png'},
+  {src: '@/assets/Wiki/Demo.png'},
+  {src: '@/assets/Wiki/Demo.png'},
+  {src: '@/assets/Wiki/Demo.png'},
+  {src: '@/assets/Wiki/Demo.png'},
+  {src: '@/assets/wiki/Demo.png'},
+])
 </script>
 
 <style scoped>
@@ -13,5 +49,11 @@
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+p {
+  color: #8F9396;
+  font-weight: 500;
+  line-height: 1.2;
 }
 </style>
