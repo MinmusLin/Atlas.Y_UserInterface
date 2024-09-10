fa45be9de67aad5b067164c969f8fd81abaa6567
MinmusLin <minmuslin@outlook.com>
2024-09-10 20:36:18 +0800

    Update DynamicDesignerHomePage.vue

--- src/pages/DynamicDesignerHomePage.vue
+++ src/pages/DynamicDesignerHomePage.vue
@@ -7,8 +7,7 @@
 
     <img src='/DesignMaterials/TitlePrompt.png' style='width: 815px; margin-bottom: 34px' alt='TitlePrompt'>
 
-    <ShadowButton width='875px' height='60px' style='margin-bottom: 10px'
-                  @click="router.push('/basic-designer/configuration')">
+    <ShadowButton width='875px' height='60px' style='margin-bottom: 10px'>
       <p style='font-size: 24px; margin-right: 10px'>START</p>
       <v-icon size='26px'>mdi-arrow-right</v-icon>
     </ShadowButton>
