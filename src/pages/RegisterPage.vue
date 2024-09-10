b01d1fd7f326b73f2a74ea5d412a5cff4747e57f
MinmusLin <minmuslin@outlook.com>
2024-09-10 20:00:18 +0800

    Update RegisterPage.vue

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -1,6 +1,31 @@
 <template>
   <div class='page-container'>
-    <p>RegisterPage</p>
+    <v-container>
+      <v-form>
+        <v-text-field label='Your Name' variant='outlined'/>
+        <v-text-field label='Email' variant='outlined'/>
+        <v-text-field label='Password' type='password' variant='outlined'/>
+        <v-btn large block color='blue'>
+          Get Started
+        </v-btn>
+
+        <div>Or</div>
+
+        <v-btn large block color='white' outlined>
+          <v-icon left>mdi-google</v-icon>
+        </v-btn>
+        <v-btn large block color='white' outlined>
+          <v-icon left>mdi-apple</v-icon>
+        </v-btn>
+        <v-btn large block color='white' outlined>
+          <v-icon left>mdi-facebook</v-icon>
+        </v-btn>
+
+        <div>
+          Already have an account? <a href='#'>Login here</a>
+        </div>
+      </v-form>
+    </v-container>
   </div>
 </template>
 
