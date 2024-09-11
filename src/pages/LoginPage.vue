6636155a04c84beb8286ac069965d1ce80b03608
Notyourbing <2252750@tongji.edu.cn>
2024-09-11 11:56:56 +0800

    Update register and log-in page.
    完善两个页面，还差谷歌/苹果/facebook的这三个logo

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,10 +1,70 @@
 <template>
   <div class='page-container'>
-    <p>LoginPage</p>
+    <v-container class="custom-container">
+      <div class="login-title">Log in to your Account</div>
+      <v-form>
+        <v-text-field class="login-input" label='Email' variant='outlined'/>  
+        
+
+        <v-text-field
+        class="login-input"
+        label='Password'
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        placeholder="Enter your password"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+
+        <div class="additional-options">
+          <label class="custom-checkbox-container">
+            <input type="checkbox" class="custom-checkbox-input">
+            <span class="custom-checkbox"></span>
+            <div class="remember-me">Remember me</div>
+          </label>
+        </div>
+
+        <v-btn class="start-button">
+          Get Started
+        </v-btn>
+
+        <div class="or-container">
+          <hr class="line" style="margin-left: 0;" />
+          <span class="or-text">Or</span>
+          <hr class="line" style="margin-right: 0;"/>
+        </div>
+
+        <div class="social-buttons">
+          <v-btn class="social-button" block color="red" outlined>
+            <v-icon left>mdi-google</v-icon>
+          </v-btn>
+          <v-btn class="social-button" block color="white" outlined>
+            <v-icon left>mdi-apple</v-icon>
+          </v-btn>
+          <v-btn class="social-button" block color="blue" outlined>
+            <v-icon left>mdi-facebook</v-icon>
+          </v-btn>
+        </div>
+
+        <div class="login-prompt">
+          New User? <a href='#' style="font-weight: 700; color: #212121;">SIGN UP HERE</a>
+        </div>
+      </v-form>
+    </v-container>
   </div>
 </template>
 
-<script setup lang='ts'>
+<script>
+export default {
+  data: () => ({  
+      visible: false,
+    }),
+  methods: {
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -13,5 +73,130 @@
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+.custom-container {
+  padding: 45px 103px 67px;
+  border: 1px solid #5182F8;
+  border-radius: 10px;
+  width: 600px;
+  height: 608px;
+}
+
+.login-title {
+  font-size: 24px;
+  font-weight: 600;
+  text-align: center;
+  margin-top: 0px;
+  margin-bottom: 40px; 
+}
+
+.additional-options {
+  display: flex;
+  align-items: center;
+  height: 23px;
+  margin-bottom: 50px;
+  justify-content: flex-start;
+}
+
+.custom-checkbox-container {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+.custom-checkbox-input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.custom-checkbox {
+  width: 22px;
+  height: 22px;
+  background-color: white;
+  border: 1px solid #EEF3FE;
+  border-radius: 3px;
+  margin-right: 8px;
+  display: inline-block;
+  position: relative;
+}
+
+.custom-checkbox-input:checked + .custom-checkbox {
+  background-color: #EEF3FE;
+}
+
+.custom-checkbox-input:checked + .custom-checkbox:after {
+  content: "";  
+  position: absolute;
+  left: 6px;
+  top: 2px;
+  width: 8px;
+  height: 12px;
+  border: solid #9E9E9E;
+  border-width: 0 2px 2px 0;
+  transform: rotate(45deg);
+}
+
+.remember-me {  
+  font-size: 12px !important;
+  font-weight: 400;
+  color: #000000;
+}
+
+.or-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 0;
+}
+
+.line {
+  border: none;
+  border-top: 1px solid #AACAF9;
+  width: 180px;
+  margin: 0 10px;
+}
+
+.or-text {
+  font-size: 12px;
+  font-weight: 400;
+  color: #212121;
+}
+
+.social-buttons {
+  display: flex;
+  justify-content: center; 
+  width: 40px;
+  margin-left: 176px;
+  margin-top: 20px;
+}
+
+.social-button {
+  width: 40px !important;
+  margin: 0 15px;
+}
+
+.start-button {
+  color: white;
+  width: 392px;
+  height: 56px !important;
+  background-color: #5182F8;
+  font-size: 16px;
+  font-weight: 600;
+  border-radius: 10px;  
+}
+
+.login-input {
+  border: 1px #5182F8 !important;
+  border-radius: 10px !important;
+}
+
+.login-prompt {
+  font-size: 13px;
+  font-weight: 400;
+  color: #212121;
+  text-align: center;
+  margin-top: 30px;
 }
 </style>
--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -1,36 +1,40 @@
 <template>
   <div class='page-container'>
-    <v-container>
+    <v-container class="custom-container">
+      <div class="login-title">Create an Account</div>
       <v-form>
-        <v-text-field label='Your Name' variant='outlined'/>
-        <v-text-field label='Email' variant='outlined'/>
-        <v-text-field label='Password' type='password' variant='outlined'/>
-        <v-btn large block color='blue'>
+        <v-text-field class="login-input" label='Your Name' variant='outlined'/>
+        <v-text-field class="login-input" label='Email' variant='outlined'/>
+        <v-text-field class="login-input" label='Password' type='password' variant='outlined'/>
+        <v-btn class="start-button">
           Get Started
         </v-btn>
 
-        <div>Or</div>
+        <div class="or-container">
+          <hr class="line" style="margin-left: 0;" />
+          <span class="or-text">Or</span>
+          <hr class="line" style="margin-right: 0;"/>
+        </div>
 
-        <v-btn large block color='white' outlined>
-          <v-icon left>mdi-google</v-icon>
-        </v-btn>
-        <v-btn large block color='white' outlined>
-          <v-icon left>mdi-apple</v-icon>
-        </v-btn>
-        <v-btn large block color='white' outlined>
-          <v-icon left>mdi-facebook</v-icon>
-        </v-btn>
+        <div class="social-buttons">
+          <v-btn class="social-button" block color="red" outlined>
+            <v-icon left>mdi-google</v-icon>
+          </v-btn>
+          <v-btn class="social-button" block color="white" outlined>
+            <v-icon left>mdi-apple</v-icon>
+          </v-btn>
+          <v-btn class="social-button" block color="blue" outlined>
+            <v-icon left>mdi-facebook</v-icon>
+          </v-btn>
+        </div>
 
-        <div>
-          Already have an account? <a href='#'>Login here</a>
+        <div class="login-prompt">
+          Already have an account? <a href='#' style="font-weight: 700; color: #212121;">LOGIN HERE</a>
         </div>
       </v-form>
     </v-container>
   </div>
 </template>
-
-<script setup lang='ts'>
-</script>
 
 <style scoped>
 .page-container {
@@ -38,5 +42,77 @@
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+.custom-container {
+  padding: 45px 103px 67px;
+  border: 1px solid #5182F8;
+  border-radius: 10px;
+  width: 600px;
+  height: 608px;
+}
+
+.login-title {
+  font-size: 24px;
+  font-weight: 600;
+  text-align: center;
+  margin-top: 0px;
+  margin-bottom: 40px; 
+}
+
+.or-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 0; /* Optional: Adjust margin as needed */
+}
+
+.line {
+  border: none;
+  border-top: 1px solid #AACAF9;
+  width: 180px;
+  margin: 0 10px; /* Space between line and text */
+}
+
+.or-text {
+  font-size: 12px;
+  font-weight: 400;
+  color: #212121;
+}
+
+.social-buttons {
+  display: flex;
+  justify-content: center; 
+  width: 40px;
+  margin-left: 176px;
+  margin-top: 20px; /* Optional: Adjust margin as needed */
+}
+
+.social-button {
+  width: 40px !important;
+  margin: 0 15px; /* Add margin for spacing between buttons */
+}
+
+.start-button {
+  color: white;
+  width: 392px;
+  height: 56px !important;
+  background-color: #5182F8;
+  font-size: 16px;
+  font-weight: 600;
+  border-radius: 10px;
+}
+
+.login-input {
+  border: 1px #5182F8 !important;
+  border-radius: 10px !important;
+}
+
+.login-prompt {
+  font-size: 13px;
+  font-weight: 400;
+  color: #212121;
+  text-align: center; /* Center align the text */
+  margin-top: 40px; /* Optional: Adjust margin as needed */
 }
 </style>
