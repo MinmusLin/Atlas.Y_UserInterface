1b03ebaad5281501a878f5f4cc3ccc3f2b6a1dd5
MinmusLin <minmuslin@outlook.com>
2024-09-14 19:16:34 +0800

    Add

--- src/pages/TeamPages/ContactUsPage.vue
+++ src/pages/TeamPages/ContactUsPage.vue
@@ -1,118 +1,100 @@
 <template>
   <div class='page-container'>
-    <h1 class ="title">Contact Us</h1>
-    <div class = "content-box">
-      <!-- Contact Information Section -->
-      <div class="section contact-information">
-        <h2 class="subtitle">Contact Information</h2>
-        <p>
-          <img src='/Icons/Mail.png' alt='LightLogo' class = "icon"/>
-          e-mail: tongji_china2019@163.com
-        </p>
-      </div>
+    <h1>Contact Us</h1>
 
-      <!-- Social Media Section -->
-      <div class="section social-media">
-        <h2 class="subtitle">Social Media</h2>
-        <p>
-          <img src='/Icons/MobilePhone.png' alt='LightLogo' class = "icon"/>
-          Official Account:
-        </p>
-        <p>
-          <img src="/Icons/QRCode.jpg" alt="LightLogo" style = "width:103px;height:98px"/>
-        </p>
-        <p>
-          <img src='/Icons/Tiktok.png' alt='LightLogo' class = "icon"/>
-          TikTok:
-          <a href="https://www.tiktok.com/@Atlas_Y_Project">https://www.tiktok.com/@Atlas_Y_Project</a>
-        </p>
-        <p>
-          <img src='/Icons/TV.png' alt='LightLogo' class = "icon"/>
-          Bilibili:
-          <a href="https://space.bilibili.com/123456">https://space.bilibili.com/123456</a>
-        </p>
-      </div>
+    <h2>Contact Information</h2>
 
-      <!-- Contact Form Section -->
-      <div class="section contact-form">
-        <h2 class="subtitle">Contact Form</h2>
-        <p>If you have any questions or feedback, please fill out the <a href="">contact form</a> and our team will get back to you as soon as possible:</p>
-        <p>
-          <img src='/Icons/PlaceHolder.png' alt='LightLogo' class = "icon"/>
-          Office Address: Tongji University, No.1239, Yangpu District, Shanghai
-        </p>
-      </div>
+    <div class='contact-content-group'>
+      <img src='/DesignMaterials/Email.png' alt='Email'>
+      <p>E-mail:&nbsp;</p>
+      <a href='mailto:tongji_china2019@163.com'>tongji_china2019@163.com</a>
+    </div>
 
+    <h2>Social Media</h2>
+
+    <div class='contact-content-group'>
+      <img src='/DesignMaterials/SmartPhone.png' alt='SmartPhone'>
+      <p>Official Account:</p>
+    </div>
+
+    <div class='contact-content-group'>
+      <img src='/DesignMaterials/TikTok.png' alt='TikTok'>
+      <p>TikTok:&nbsp;</p>
+      <a href='https://www.tiktok.com/@Atlas_Y_Project' target='_blank'>https://www.tiktok.com/@Atlas_Y_Project</a>
+    </div>
+
+    <div class='contact-content-group'>
+      <img src='/DesignMaterials/Bilibili.png' alt='Bilibili'>
+      <p>Bilibili:&nbsp;</p>
+      <a href='https://space.bilibili.com/123456' target='_blank'>https://space.bilibili.com/123456</a>
+    </div>
+
+    <h2>Contact Form</h2>
+
+    <p style='margin-top: 21px; margin-bottom: 6px'>
+      If you have any questions or feedback, please fill out the
+      <a @click="router.push('/contact-us/form')">contact form</a>
+      and our team will get back to you as soon as possible.
+    </p>
+
+    <div class='contact-content-group'>
+      <img src='/DesignMaterials/MapMarker.png' alt='MapMarker'>
+      <p>Office Address: Tongji University, No.1239, Yangpu District, Shanghai</p>
     </div>
   </div>
 </template>
 
 <script setup lang='ts'>
-import InfoTooltip from "@/components/InfoTooltip.vue";
-import Dialog from "@/components/Dialog.vue";
+import {useRouter} from 'vue-router'
+
+const router = useRouter()
 </script>
 
 <style scoped>
-
 .page-container {
   display: flex;
   flex-direction: column;
-  justify-content: left;
+  justify-content: center;
+  align-items: flex-start;
+  text-align: left;
+  padding-left: 20px;
 }
 
-.title {
-  margin-top:37px;
-  font-size: 40px;
-  color: #5182F8;
-}
-.subtitle {
-  font-size: 16px;
-  margin-bottom: 16px;
-}
-.content-box {
-  margin-top: 57px;
-  max-width: 787px;
-  max-height: 160px;
-  font-weight:bold;
+.contact-content-group {
+  display: flex;
+  margin-top: 15px;
 }
 
-.section {
-  margin-bottom: 48px;
-}
-
-.contact-information p,
-.social-media p {
-  font-size: 16px;
-  margin-bottom: 16px;
-}
-
-.contact-form p {
-  font-size: 16px;
-  font-weight: bold;
-}
-
-.contact-form p:first-of-type {
-  margin-bottom: 12px;
+.contact-content-group img {
+  height: 24px;
+  width: 24px;
+  margin-right: 8px;
 }
 
 a {
   color: #5182F8;
-  margin-bottom: 12px;
-  display: inline-block;
-  font-weight: bold;
-  text-decoration: underline;
+  font-weight: 600;
+  text-decoration: none;
+  cursor: pointer;
 }
 
-a:hover {
-  text-decoration: underline;
+p {
+  font-weight: 600;
+  color: #2F3235;
 }
-.contact-form p:last-of-type {
-  margin-top: 12px;
-  font-weight: bold;
+
+h2 {
+  font-weight: 700;
+  font-size: 16px;
+  margin-top: 45px;
+  color: #2F3235;
+  margin-bottom: -2px;
 }
-.icon{
-  width: 22px;
-  height: 22px;
-  vertical-align: middle;
+
+h1 {
+  color: #5182F8;
+  font-size: 40px;
+  font-weight: 600;
+  margin-bottom: 2px;
 }
 </style>
