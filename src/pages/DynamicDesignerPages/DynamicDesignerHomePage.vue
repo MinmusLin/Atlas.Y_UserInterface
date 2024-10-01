7ec96183317a7bdf27c661f176d7a57d18359adb
MinmusLin <minmuslin@outlook.com>
2024-10-02 06:59:36 +0800

    +

--- src/pages/DynamicDesignerPages/DynamicDesignerHomePage.vue
+++ src/pages/DynamicDesignerPages/DynamicDesignerHomePage.vue
@@ -35,27 +35,6 @@
       <el-table-column width='190' prop='pdb' label='PDB'/>
       <el-table-column width='190' prop='subcellularPosition' label='SUBCELLULAR POSITION'/>
     </el-table>
-
-    <v-dialog v-model='showDialog' width='85%' min-width='1270px' persistent transition='dialog-bottom-transition'>
-      <v-card class='cookies-dialog'>
-        <v-card-text class='d-flex align-center'>
-          <span class='cookie-icon'>
-            <v-icon size='40px' color='#AACAF9'>mdi-cookie</v-icon>
-          </span>
-          <span style='font-size: 18px'>
-            Our website uses cookies. By continuing to navigate, we assume your permission to deploy cookies as detailed in our Privacy Policy.
-          </span>
-          <v-btn class='accept-btn' @click='showDialog=false'>
-            <v-icon size='22px' style='margin-right: 4px'>mdi-cookie</v-icon>
-            Accept cookies
-          </v-btn>
-          <v-btn class='decline-btn' @click='showDialog=false'>
-            Decline
-          </v-btn>
-          <v-btn class='close-btn' icon='mdi-close' @click='showDialog=false'/>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
   </div>
 
   <Dialog v-model='showContinueDialog' style='padding: 14px 40px 0 40px; width: 543px; height: 299px'>
@@ -107,7 +86,6 @@ import Dialog from '@/components/Dialog.vue'
 import DefaultButton from '@/components/DefaultButton.vue'
 
 const router = useRouter()
-const showDialog = ref(true)
 const showContinueDialog = ref(false)
 const headerCellStyle = () => ({color: '#2F62D7', textAlign: 'center', fontSize: '12px', paddingBottom: '2px'})
 const cellStyle = () => ({color: '#2F3235', textAlign: 'center', fontSize: '16px'})
@@ -192,74 +170,6 @@ const tableData = ref([
   border-top-right-radius: 5px !important;
   border-bottom-right-radius: 5px !important;
   border-left: none !important;
-}
-
-.d-flex {
-  display: flex;
-  align-items: center;
-}
-
-.cookie-icon {
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #F6FBFF;
-  border-radius: 50%;
-  padding: 10px;
-  margin-left: 36px;
-  margin-right: 26px
-}
-
-.accept-btn {
-  background-color: #5182F8;
-  color: white;
-  border-radius: 10px;
-  width: 212px;
-  height: 52px !important;
-  box-shadow: none;
-  margin-left: 30px;
-  text-transform: none;
-  letter-spacing: 0;
-  font-size: 16px;
-}
-
-.accept-btn:hover {
-  box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.15)
-}
-
-.decline-btn {
-  border: 2px solid #5182F8;
-  color: #2F3235;
-  border-radius: 10px;
-  width: 110px;
-  height: 52px !important;
-  box-shadow: none;
-  margin-right: 28px;
-  margin-left: 28px;
-  text-transform: none;
-  letter-spacing: 0;
-  font-size: 16px;
-}
-
-.decline-btn:hover {
-  box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.15)
-}
-
-.close-btn {
-  color: #170F49;
-  border: none;
-  box-shadow: none;
-}
-
-.close-btn:hover {
-  box-shadow: none;
-}
-
-.cookies-dialog {
-  border-radius: 10px;
-  padding-top: 38px;
-  padding-bottom: 30px;
-  transform: translateY(calc(50vh - 130px)) translateX(42px);
 }
 
 .icon {
