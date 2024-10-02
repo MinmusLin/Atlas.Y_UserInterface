09e25ef0aeef7ec1f304b54803f896435f624892
MinmusLin <minmuslin@outlook.com>
2024-10-02 15:29:21 +0800

    Update FAQsPage.vue

--- src/pages/SupportPages/FAQsPage.vue
+++ src/pages/SupportPages/FAQsPage.vue
@@ -1,6 +1,8 @@
 <template>
   <div class='page-container'>
-    <p>FAQsPage</p>
+    <div class='container'>
+
+    </div>
   </div>
 </template>
 
@@ -9,8 +11,13 @@
 
 <style scoped>
 .page-container {
+  width: 100%;
+  height: 100vh;
+  overflow-y: scroll;
+}
+
+.container {
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
